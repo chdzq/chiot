561774e01b6ffb5821b1cf9d0bac2346c7589d44
zhangqi <zhangqi_248@qq.com>
2024-11-30 03:41:30 +0800

    Feat: 增加Common redis

---
+++ common/common-redis/src/main/java/org/chdzq/common/redis/config/RedisConfig.java
@@ -0,0 +1,41 @@
+package org.chdzq.common.redis.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializer;
+
+/**
+ * Redis 配置
+ *
+ * @author chdzq
+ * @version 1.0
+ * @date 2024/11/29 23:01
+ */
+@Configuration
+public class RedisConfig {
+
+    /**
+     * 自定义 RedisTemplate
+     * 修改 Redis 序列化方式，默认 JdkSerializationRedisSerializer
+     *
+     * @param redisConnectionFactory {@link RedisConnectionFactory}
+     * @return {@link RedisTemplate}
+     */
+//    @Bean
+//    public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+//
+//        RedisTemplate<Object, Object> redisTemplate = new RedisTemplate<>();
+//        redisTemplate.setConnectionFactory(redisConnectionFactory);
+//
+//        redisTemplate.setKeySerializer(RedisSerializer.string());
+//        redisTemplate.setValueSerializer(RedisSerializer.json());
+//
+//        redisTemplate.setHashKeySerializer(RedisSerializer.string());
+//        redisTemplate.setHashValueSerializer(RedisSerializer.json());
+//
+//        redisTemplate.afterPropertiesSet();
+//        return redisTemplate;
+//    }
+}
